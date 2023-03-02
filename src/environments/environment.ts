5115413df293fe538903824a7797500f40734358
omarch21 <omarch641@gmail.com>
2023-03-01 21:27:13 -0600

    project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDpZMgvVzsYUZL7iUhU63g3V5DWrd2vXnM",
+    authDomain: "notesxplus.firebaseapp.com",
+    projectId: "notesxplus",
+    storageBucket: "notesxplus.appspot.com",
+    messagingSenderId: "124260459995",
+    appId: "1:124260459995:web:c41e044e8e5cd94b0d33fe"
+  }
 };
 
 /*
