3345002a27d781175ee35c2160ce6666bb209f04
omarch21 <omarch641@gmail.com>
2023-03-04 22:58:17 -0600

    stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDpZMgvVzsYUZL7iUhU63g3V5DWrd2vXnM",
+    authDomain: "notesxplus.firebaseapp.com",
+    projectId: "notesxplus",
+    storageBucket: "notesxplus.appspot.com",
+    messagingSenderId: "124260459995",
+    appId: "1:124260459995:web:c41e044e8e5cd94b0d33fe"
+  }
 };
